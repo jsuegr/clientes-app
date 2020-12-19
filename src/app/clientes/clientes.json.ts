4ea56e472e567570316be097e1fa036c127912b9
unknown <jsuegr@gmail.com>
2020-12-19 14:48:31 -0600

    Creando archivo clientes.json.ts con la lista de objetos

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,110 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+
+  {
+    "id": 0,
+    "nombre": "Brittany",
+    "apellido": "Ortega",
+    "createAt": "2019-11-20T09:53:29 +06:00",
+    "email": "brittanyortega@quintity.com"
+  },
+  {
+    "id": 1,
+    "nombre": "Ramos",
+    "apellido": "Washington",
+    "createAt": "2020-03-08T04:48:00 +06:00",
+    "email": "ramoswashington@quintity.com"
+  },
+  {
+    "id": 2,
+    "nombre": "Garrison",
+    "apellido": "Rose",
+    "createAt": "2016-11-29T09:45:42 +06:00",
+    "email": "garrisonrose@quintity.com"
+  },
+  {
+    "id": 3,
+    "nombre": "Beryl",
+    "apellido": "Stewart",
+    "createAt": "2017-08-26T12:25:45 +05:00",
+    "email": "berylstewart@quintity.com"
+  },
+  {
+    "id": 4,
+    "nombre": "Anderson",
+    "apellido": "Serrano",
+    "createAt": "2015-04-27T11:27:14 +05:00",
+    "email": "andersonserrano@quintity.com"
+  },
+  {
+    "id": 5,
+    "nombre": "Vargas",
+    "apellido": "Gardner",
+    "createAt": "2020-05-10T02:53:42 +05:00",
+    "email": "vargasgardner@quintity.com"
+  },
+  {
+    "id": 6,
+    "nombre": "Ann",
+    "apellido": "Fleming",
+    "createAt": "2020-10-20T11:11:56 +05:00",
+    "email": "annfleming@quintity.com"
+  },
+  {
+    "id": 7,
+    "nombre": "Cathy",
+    "apellido": "Ramsey",
+    "createAt": "2019-01-30T12:41:37 +06:00",
+    "email": "cathyramsey@quintity.com"
+  },
+  {
+    "id": 8,
+    "nombre": "Stacy",
+    "apellido": "Orr",
+    "createAt": "2018-06-16T10:25:13 +05:00",
+    "email": "stacyorr@quintity.com"
+  },
+  {
+    "id": 9,
+    "nombre": "Sally",
+    "apellido": "Guzman",
+    "createAt": "2014-07-21T08:37:17 +05:00",
+    "email": "sallyguzman@quintity.com"
+  },
+  {
+    "id": 10,
+    "nombre": "Vinson",
+    "apellido": "Fitzgerald",
+    "createAt": "2015-05-25T02:12:34 +05:00",
+    "email": "vinsonfitzgerald@quintity.com"
+  },
+  {
+    "id": 11,
+    "nombre": "Lambert",
+    "apellido": "Peters",
+    "createAt": "2014-08-22T07:15:42 +05:00",
+    "email": "lambertpeters@quintity.com"
+  },
+  {
+    "id": 12,
+    "nombre": "Contreras",
+    "apellido": "Jacobs",
+    "createAt": "2019-09-28T02:06:15 +05:00",
+    "email": "contrerasjacobs@quintity.com"
+  },
+  {
+    "id": 13,
+    "nombre": "Dena",
+    "apellido": "Beasley",
+    "createAt": "2015-05-24T02:48:06 +05:00",
+    "email": "denabeasley@quintity.com"
+  },
+  {
+    "id": 14,
+    "nombre": "Hillary",
+    "apellido": "Johns",
+    "createAt": "2019-01-10T12:41:53 +06:00",
+    "email": "hillaryjohns@quintity.com"
+  }
+];
